52eb63995e7fd13b8af54d9c002090b562b22d4f
Joshua Dierickse <joshua.dierickse@gmail.com>
2024-08-18 02:02:41 -0400

    Update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth/next";
-import GitHubProvider from "next-auth/providers/github"
+import GitHubProvider from "next-auth/providers/github";
 
-export const authOptions = {
+const authOptions = {
     providers: [
         GitHubProvider({
-            clientId: process.env.GITHUB_ID ?? "", 
+            clientId: process.env.GITHUB_ID ?? "",
             clientSecret: process.env.GITHUB_SECRET ?? "",
         }),
     ],
